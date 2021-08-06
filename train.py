c6205b069fb7e5b745fc05248aa76bc3192a8315
hhurchand <h.hurchand@gmail.com>
2021-08-06 15:21:05 -0400

    Update train.py

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ from sklearn.impute import SimpleImputer
 import pickle
 
 ###
-#### import neptune.new as neptune
-#### from neptune.new.types import File
+import neptune.new as neptune
+from neptune.new.types import File
 #### run = neptune.init(api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2ZDIwNGI1YS02NDZiLTQ2ODctYjcxOS0xNDIxMzQzMWJjM2IifQ==" ,project='h.hurchand/BostonDataBDEB')
 df = pd.read_csv('BostonData.csv',header=0)
 
