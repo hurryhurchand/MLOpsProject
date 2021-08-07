156706057d2ce96cbb16960183416950348531e9
hhurchand <h.hurchand@gmail.com>
2021-08-06 21:15:23 -0400

    Update train.py

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ y = df0["MEDV"]
 # In[15]:
 
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
 
 
 # In[16]:
